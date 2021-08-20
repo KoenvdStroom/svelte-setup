6e0d5fb81e073487447577368341eeb21f9f2379
Koen van der Stroom <koen@kvds.dev>
2021-08-20 12:25:27 +0200

    Update

--- setup.sh
+++ setup.sh
@@ -6,18 +6,13 @@ yarn install
 
 yarn add -D autoprefixer postcss-cli tailwindcss concurrently cross-env
 
+mkdir src/styles
+
 npx tailwindcss init tailwind.config.cjs
 
-echo "module.exports = {
-  plugins: {
-    autoprefixer: {},
-    tailwindcss: {},
-  },
-}" > postcss.config.cjs
+echo "module.exports = {plugins: {autoprefixer: {},tailwindcss: {},},}" > postcss.config.cjs
 
-echo "@tailwind base;
-@tailwind components;
-@tailwind utilities;" > src/styles/tailwind.css
+echo "@tailwind base; @tailwind components; @tailwind utilities;" > src/styles/tailwind.css
 
 curl "https://raw.githubusercontent.com/KoenvdStroom/svelte-setup/master/package.json" > package.json
 
